71e8ea5395b2d1eaee4a5328fb25833e02cdd32c
indratwks <indra.dutta@thoughtworks.com>
2022-04-03 10:35:49 +0200

    adding tag to product

--- src/product.js
+++ src/product.js
@@ -1,8 +1,9 @@
 export class Product {
-  constructor({id, name, type, price}) {
+  constructor({id, name, type, price, tag}) {
     this.id = id
     this.name = name
     this.type = type
     this.price = price
+	this.tag = tag	
   }
 }
